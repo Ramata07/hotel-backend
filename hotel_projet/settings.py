7601f8638ba16569dac868c94c1e2efc45a0a607
Ramata07 <nianeramata0@gmail.com>
2026-02-07 23:24:43 +0000

    Essai2 cloudinary

--- hotel_projet/settings.py
+++ hotel_projet/settings.py
@@ -63,14 +63,16 @@ REST_FRAMEWORK = {
 
 
 import cloudinary
+import os
 
-if os.environ.get("CLOUDINARY_CLOUD_NAME"):
-    cloudinary.config(
-        cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-        api_key=os.environ.get("CLOUDINARY_API_KEY"),
-        api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-    )
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+)
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
--- hotell/serializers.py
+++ hotell/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
 from .models import Hotel
 
+
 class HotelSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Hotel
         fields = '__all__'
+
+    def get_image(self, obj):
+        if not obj.image:
+            return None
+        try:
+            url = obj.image.url
+            # Si l'URL est relative, la rendre absolue
+            if url and url.startswith('/'):
+                request = self.context.get('request')
+                if request:
+                    return request.build_absolute_uri(url)
+            return url  # Déjà une URL complète (ex: Cloudinary)
+        except (ValueError, AttributeError):
+            return None
