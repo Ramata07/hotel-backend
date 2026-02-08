c06cf3cc8149ccba546a016afee2c4a59dbe9ce5
Ramata07 <nianeramata0@gmail.com>
2026-02-08 19:02:55 +0000

    Changement de port

--- hotel_projet/settings.py
+++ hotel_projet/settings.py
@@ -191,6 +191,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # pour développement
+    "http://localhost:3001",  # pour développement
     "https://ton-frontend.vercel.app",  # ton futur lien Vercel
 ]
