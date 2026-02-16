15ab826471f06a3d6084b30f571c87483f610b83
Ramata07 <nianeramata0@gmail.com>
2026-02-16 09:37:11 +0000

    update cors origin

--- hotel_projet/settings.py
+++ hotel_projet/settings.py
@@ -220,10 +220,11 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    # "https://hotel-frontend-qhlqwjwsx-ramata07s-projects.vercel.app",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://.*\.vercel\.app$",
+    r"^https://.*vercel\.app$",
 ]
   
 
