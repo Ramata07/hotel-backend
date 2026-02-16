647e6f4bcec1ff76c884258b9b9fd59de47a4a26
Ramata07 <nianeramata0@gmail.com>
2026-02-16 12:34:07 +0000

    update 2

--- hotel_projet/settings.py
+++ hotel_projet/settings.py
@@ -218,8 +218,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+   # "http://localhost:3000",
+  #  "http://127.0.0.1:3000",
     "https://hotel-frontend-swart-omega.vercel.app",
     "https://hotel-frontend-qhlqwjwsx-ramata07s-projects.vercel.app",  
 ]
