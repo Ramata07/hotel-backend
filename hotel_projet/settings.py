3e93edcb91108fba1d0c292f8adb323af2c061d7
Ramata07 <nianeramata0@gmail.com>
2026-02-17 14:21:54 +0000

    link vercel

--- hotel_projet/settings.py
+++ hotel_projet/settings.py
@@ -226,7 +226,7 @@ CORS_ALLOWED_ORIGINS = [
     #"http://localhost:3000",
     #"http://127.0.0.1:3000",
     "https://hotel-frontend-swart-omega.vercel.app",
-    "https://hotel-frontend-qhlqwjwsx-ramata07s-projects.vercel.app",  
+    #"https://hotel-frontend-qhlqwjwsx-ramata07s-projects.vercel.app",  
 ]
 
 CORS_ALLOW_HEADERS = [
