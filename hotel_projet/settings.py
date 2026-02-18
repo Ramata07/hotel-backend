ade4691d57501ac8447c56a478a1bc65dc0de405
Ramata07 <nianeramata0@gmail.com>
2026-02-18 21:10:25 +0000

    reset password

--- hotel_projet/settings.py
+++ hotel_projet/settings.py
@@ -274,7 +274,7 @@ DJOSER = {
     'SET_PASSWORD_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_RETYPE': True,
     'SEND_ACTIVATION_EMAIL': True,
-    'SEND_PASSWORD_RESET_EMAIL': False,
+    'SEND_PASSWORD_RESET_EMAIL': True,
     'USER_ID_FIELD': 'id',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'PASSWORD_RESET_CONFIRM_URL': 'password-reset/{uid}/{token}',
